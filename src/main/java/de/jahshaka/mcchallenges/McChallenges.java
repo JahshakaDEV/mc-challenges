9e3849fcf5ccb7dc5393b0dfba2e4853006e086c
Jannik Oeschger <oeschgerjannik@gmail.com>
2022-08-09 23:55:25 +0200

    📄 Added copyright info to main class

--- src/main/java/de/jahshaka/mcchallenges/McChallenges.java
+++ src/main/java/de/jahshaka/mcchallenges/McChallenges.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright (C) 2022 Jannik Oeschger. - All Rights Reserved
+ *
+ * Unauthorized copying or redistribution of this file in source and binary forms via any medium
+ * is strictly prohibited.
+ */
+
 package de.jahshaka.mcchallenges;
 
 import org.bukkit.plugin.java.JavaPlugin;
